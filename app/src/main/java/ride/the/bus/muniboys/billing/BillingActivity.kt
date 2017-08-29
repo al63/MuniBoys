52c3633acd30676992369b156ef4d9308349931f
alec-lee <alec.lee@justin.tv>
2017-08-29 16:21:26 -0700

    billing activity for testing...

---
+++ app/src/main/java/ride/the/bus/muniboys/billing/BillingActivity.kt
@@ -0,0 +1,22 @@
+package ride.the.bus.muniboys.billing
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import ride.the.bus.muniboys.R
+
+class BillingActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_billing)
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+}
+
